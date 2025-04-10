834caaffee1f6e6759320f33d4a605df5104be0d
Rishabhraj Srivastava <rishabhraj.srivastava2021@vitstudent.ac.in>
2025-04-10 10:43:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import logging
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from processors.data_integrator import AgriDataIntegrator
+import logging
 
 logging.basicConfig(
     level=logging.INFO,
